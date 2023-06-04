0f127b0b943c9d803e970e07ffe085d73de4f651
Vondell Naicker <vondellnaicker@outlook.com>
2023-06-04 17:24:00 +0200

    LoginForm Code Done

--- LoginForm.Designer.cs
+++ LoginForm.Designer.cs
@@ -30,6 +30,12 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.rbtnManager = new System.Windows.Forms.RadioButton();
+            this.rbtnCoach = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,27 +46,108 @@
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(872, 610);
+            this.pictureBox1.Size = new System.Drawing.Size(869, 621);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(63, 555);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(408, 453);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUserName.Location = new System.Drawing.Point(332, 344);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(223, 29);
+            this.txtUserName.TabIndex = 3;
+            this.txtUserName.Text = "ManagerID";
+            this.txtUserName.MouseEnter += new System.EventHandler(this.txtUserName_MouseEnter);
+            this.txtUserName.MouseLeave += new System.EventHandler(this.txtUserName_MouseLeave);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(332, 397);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(223, 29);
+            this.txtPassword.TabIndex = 4;
+            this.txtPassword.Text = "Password";
+            this.txtPassword.MouseEnter += new System.EventHandler(this.txtPassword_MouseEnter);
+            this.txtPassword.MouseLeave += new System.EventHandler(this.txtPassword_MouseLeave);
+            // 
+            // rbtnManager
+            // 
+            this.rbtnManager.AutoSize = true;
+            this.rbtnManager.Checked = true;
+            this.rbtnManager.Location = new System.Drawing.Point(344, 281);
+            this.rbtnManager.Name = "rbtnManager";
+            this.rbtnManager.Size = new System.Drawing.Size(67, 17);
+            this.rbtnManager.TabIndex = 5;
+            this.rbtnManager.TabStop = true;
+            this.rbtnManager.Text = "Manager";
+            this.rbtnManager.UseVisualStyleBackColor = true;
+            this.rbtnManager.CheckedChanged += new System.EventHandler(this.rbtnManager_CheckedChanged);
+            // 
+            // rbtnCoach
+            // 
+            this.rbtnCoach.AutoSize = true;
+            this.rbtnCoach.Location = new System.Drawing.Point(462, 281);
+            this.rbtnCoach.Name = "rbtnCoach";
+            this.rbtnCoach.Size = new System.Drawing.Size(56, 17);
+            this.rbtnCoach.TabIndex = 6;
+            this.rbtnCoach.Text = "Coach";
+            this.rbtnCoach.UseVisualStyleBackColor = true;
+            this.rbtnCoach.CheckedChanged += new System.EventHandler(this.rbtnCoach_CheckedChanged);
             // 
             // LoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(872, 610);
+            this.ClientSize = new System.Drawing.Size(869, 621);
+            this.Controls.Add(this.rbtnCoach);
+            this.Controls.Add(this.rbtnManager);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.btnExit);
             this.Controls.Add(this.pictureBox1);
             this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Loginform";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.LoginForm_FormClosing);
             this.Load += new System.EventHandler(this.LoginForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.RadioButton rbtnManager;
+        private System.Windows.Forms.RadioButton rbtnCoach;
     }
 }
 
--- LoginForm.cs
+++ LoginForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ namespace ISTN3AS_M2
 {
     public partial class LoginForm : Form
     {
+        private SqlConnection connection;
+        private string connectString = "Data Source=146.230.177.46;Initial Catalog = GroupWst13; Persist Security Info=True;User ID = GroupWst13; Password=kqf42";
         public LoginForm()
         {
             InitializeComponent();
@@ -19,7 +22,111 @@ namespace ISTN3AS_M2
 
         private void LoginForm_Load(object sender, EventArgs e)
         {
-            int test = 0;
+            connection = new SqlConnection(connectString);
+            connection.Open();
+        }
+
+        private void LoginForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            connection.Close();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUserName.Text;
+            string password = txtPassword.Text;
+
+            string query = "";
+
+            if (rbtnManager.Checked)
+            {
+                query = "SELECT COUNT(*) FROM Manager WHERE ManagerID = @Username AND Password = @Password";
+            }
+            else if (rbtnCoach.Checked)
+            {
+                query = "SELECT COUNT(*) FROM Coach WHERE CoachID = @Username AND Password = @Password";
+            }
+    
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@Username", username);
+            command.Parameters.AddWithValue("@Password", password);
+
+            int result = (int)command.ExecuteScalar();
+
+            if ((result > 0) && (rbtnManager.Checked))
+            {
+                // frmMenu form = new frmMenu();
+                //  form.ShowDialog();
+                //    this.Hide();
+                MessageBox.Show("Manager Login");
+            }
+            else if ((result > 0) && (rbtnCoach.Checked))
+            {
+                MessageBox.Show("Coach Login");
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password!\nPlease enter a valid Username or Password.", "Unsuccessful Login", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void txtUserName_MouseEnter(object sender, EventArgs e)
+        {
+            if ((txtUserName.Text == "ManagerID") || (txtUserName.Text == "CoachID"))
+            {
+                txtUserName.Text = "";
+                txtUserName.Focus();
+            }  
+        }
+
+        private void txtUserName_MouseLeave(object sender, EventArgs e)
+        {
+            if ((txtUserName.Text == "") && (rbtnManager.Checked))
+            {
+                txtUserName.Text = "ManagerID";
+            }
+            else if ((txtUserName.Text == "") && (rbtnCoach.Checked))
+            {
+                txtUserName.Text = "CoachID";
+            }
+        }
+
+        private void txtPassword_MouseEnter(object sender, EventArgs e)
+        {
+            if (txtPassword.Text == "Password") 
+            {
+                txtPassword.Text = "";
+                txtPassword.Focus();
+            }
+        }
+
+        private void txtPassword_MouseLeave(object sender, EventArgs e)
+        {
+            if (txtPassword.Text == "")
+            {
+                txtPassword.Text = "Password";
+            }
+        }
+
+        private void rbtnManager_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbtnManager.Checked)
+            {
+                txtUserName.Text = "ManagerID";
+            }
+        }
+
+        private void rbtnCoach_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbtnCoach.Checked)
+            {
+                txtUserName.Text = "CoachID";
+            }
         }
     }
 }
