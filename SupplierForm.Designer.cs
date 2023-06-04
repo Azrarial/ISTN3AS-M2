76eb5f99092b062574820e256c9360d0517d94b3
Ashivaar <ashivaarharipersad@gmail.com>
2023-06-04 21:20:19 +0200

    Ash's Classes

---
+++ SupplierForm.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace ISTN3AS_M2
+{
+    partial class SupplierForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dbSupplier = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtCellNum = new System.Windows.Forms.TextBox();
+            this.txtSuppName = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtSupplierID = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.txtUpdateEmail = new System.Windows.Forms.TextBox();
+            this.txtUpdateContactNum = new System.Windows.Forms.TextBox();
+            this.txtUpdateName = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dbSupplier)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(386, 35);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(266, 30);
+            this.txtSearch.TabIndex = 7;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(38, 31);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(258, 25);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Search by Supplier Name";
+            // 
+            // dbSupplier
+            // 
+            this.dbSupplier.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dbSupplier.Location = new System.Drawing.Point(43, 97);
+            this.dbSupplier.Name = "dbSupplier";
+            this.dbSupplier.RowHeadersWidth = 51;
+            this.dbSupplier.RowTemplate.Height = 24;
+            this.dbSupplier.Size = new System.Drawing.Size(1051, 150);
+            this.dbSupplier.TabIndex = 5;
+            this.dbSupplier.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dbSupplier_RowHeaderMouseClick);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.txtEmail);
+            this.groupBox2.Controls.Add(this.txtCellNum);
+            this.groupBox2.Controls.Add(this.txtSuppName);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.txtSupplierID);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(548, 282);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(476, 350);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Add";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(307, 297);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(139, 36);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Add";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(215, 239);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(231, 30);
+            this.txtEmail.TabIndex = 8;
+            // 
+            // txtCellNum
+            // 
+            this.txtCellNum.Location = new System.Drawing.Point(215, 184);
+            this.txtCellNum.Name = "txtCellNum";
+            this.txtCellNum.Size = new System.Drawing.Size(231, 30);
+            this.txtCellNum.TabIndex = 7;
+            // 
+            // txtSuppName
+            // 
+            this.txtSuppName.Location = new System.Drawing.Point(215, 125);
+            this.txtSuppName.Name = "txtSuppName";
+            this.txtSuppName.Size = new System.Drawing.Size(231, 30);
+            this.txtSuppName.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(24, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 62);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Generate SupplierID";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtSupplierID
+            // 
+            this.txtSupplierID.Location = new System.Drawing.Point(215, 39);
+            this.txtSupplierID.Name = "txtSupplierID";
+            this.txtSupplierID.Size = new System.Drawing.Size(231, 30);
+            this.txtSupplierID.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 239);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 25);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Email";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 189);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(168, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Contact Number";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Supplier Name";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.txtUpdateEmail);
+            this.groupBox1.Controls.Add(this.txtUpdateContactNum);
+            this.groupBox1.Controls.Add(this.txtUpdateName);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(43, 282);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(477, 350);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Update";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(316, 233);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(139, 36);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Update";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // txtUpdateEmail
+            // 
+            this.txtUpdateEmail.Location = new System.Drawing.Point(217, 170);
+            this.txtUpdateEmail.Name = "txtUpdateEmail";
+            this.txtUpdateEmail.Size = new System.Drawing.Size(238, 30);
+            this.txtUpdateEmail.TabIndex = 6;
+            // 
+            // txtUpdateContactNum
+            // 
+            this.txtUpdateContactNum.Location = new System.Drawing.Point(217, 112);
+            this.txtUpdateContactNum.Name = "txtUpdateContactNum";
+            this.txtUpdateContactNum.Size = new System.Drawing.Size(238, 30);
+            this.txtUpdateContactNum.TabIndex = 5;
+            // 
+            // txtUpdateName
+            // 
+            this.txtUpdateName.Location = new System.Drawing.Point(217, 58);
+            this.txtUpdateName.Name = "txtUpdateName";
+            this.txtUpdateName.Size = new System.Drawing.Size(238, 30);
+            this.txtUpdateName.TabIndex = 4;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(17, 166);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 25);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Email";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(17, 112);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(168, 25);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Contact Number";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 58);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(154, 25);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Supplier Name";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(1060, 282);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(487, 200);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Delete";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 36);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Delete";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(404, 25);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Click button below to delete selected row";
+            // 
+            // SupplierForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1711, 736);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.dbSupplier);
+            this.Name = "SupplierForm";
+            this.Text = "SupplierForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dbSupplier)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DataGridView dbSupplier;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtCellNum;
+        private System.Windows.Forms.TextBox txtSuppName;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtSupplierID;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox txtUpdateEmail;
+        private System.Windows.Forms.TextBox txtUpdateContactNum;
+        private System.Windows.Forms.TextBox txtUpdateName;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ SupplierForm.cs
@@ -0,0 +1,95 @@
+﻿using ISTN3AS_M2.GroupWst13DataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ISTN3AS_M2
+{
+    public partial class SupplierForm : Form
+    {
+        public SupplierForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string SupplierID = txtSupplierID.Text;
+            string SupplierName = txtSuppName.Text;
+            string SupplierCell = txtCellNum.Text;
+            string SupplierEmail = txtEmail.Text;
+
+            //supplierTableAdapter.Insert(SupplierID, SupplierName, SupplierCell, SupplierEmail);
+            //this.supplierTableAdapter.Fill(this.dsBox360.Supplier);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           // dsBox360.Supplier.Rows[dbSupplier.CurrentRow.Index].Delete();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            Random random = new Random();
+            string id = "";
+
+            int colIndex = 0;
+            bool bFound = false;
+            bool isFound = false;
+
+            while (bFound == false)
+            {
+                isFound = false;
+                id = (random.Next(10000, 99999)).ToString();
+                foreach (DataGridViewRow row in dbSupplier.Rows)
+                {
+                    if (row.Cells[colIndex].Value != null && getIntID(row.Cells[colIndex].Value.ToString()) == "SP" + id)
+                    {
+                        isFound = true;
+                    }
+                }
+
+                if (isFound == false)
+                {
+                    bFound = true;
+                }
+            }
+
+            txtSupplierID.Text = "SP" + id;
+        }
+
+        private string getIntID(string id)
+        {
+            string temp = id.Substring(2);
+
+            return temp;
+        }
+
+        private void dbSupplier_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            txtUpdateName.Text = dbSupplier.CurrentRow.Cells[1].Value.ToString();
+            txtUpdateContactNum.Text = dbSupplier.CurrentRow.Cells[2].Value.ToString();
+            txtUpdateEmail.Text = dbSupplier.CurrentRow.Cells[3].Value.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+          //  supplierTableAdapter.UpdateSupplierName(txtUpdateName.Text, dbSupplier.CurrentRow.Cells[0].Value.ToString(), dbSupplier.CurrentRow.Cells[0].Value.ToString());
+          //  supplierTableAdapter.UpdateContactNumber(txtUpdateContactNum.Text, dbSupplier.CurrentRow.Cells[0].Value.ToString(), dbSupplier.CurrentRow.Cells[0].Value.ToString());
+          //  supplierTableAdapter.UpdateEmail(txtUpdateEmail.Text, dbSupplier.CurrentRow.Cells[0].Value.ToString(), dbSupplier.CurrentRow.Cells[0].Value.ToString());
+          //  this.supplierTableAdapter.Fill(this.dsBox360.Supplier);
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            //supplierTableAdapter.FillBySupplierName(dsBox360.Supplier, txtSearch.Text);
+        }
+    }
+}
